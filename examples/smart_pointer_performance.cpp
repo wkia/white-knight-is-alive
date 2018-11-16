ae2501b85a888d075242b5cd0179ece36c183813
w.k.i.a <wkia@users.noreply.github.com>
2018-11-16 12:21:56 +0300

    Update smart_pointer_performance.cpp

--- examples/smart_pointer_performance.cpp
+++ examples/smart_pointer_performance.cpp
@@ -13,9 +13,9 @@ void runTest(const std::string &title, F testFunc)
 {
 	auto start = std::chrono::system_clock::now();
 
-	for (long long i = 0; i < Count; ++i)
+	for (size_t i = 0; i < Count; ++i)
 	{
-		testFunc(T(i));
+		testFunc(i);
 	}
 
 	std::chrono::duration<double> dur = std::chrono::system_clock::now() - start;
@@ -26,11 +26,11 @@ int main()
 {
 	std::cout << "Number of iterations: " << Count << std::endl << std::endl;
 
-	runTest("Raw ptr            ", [](const T &i) { T* tmp(new T(i)); delete tmp; });
-	runTest("std::shared_ptr    ", [](const T &i) { std::shared_ptr<T> tmp(new T(i)); });
-	runTest("std::make_shared   ", [](const T &i) { auto tmp = std::make_shared<T>(i); });
-	runTest("std::unique_ptr    ", [](const T &i) { std::unique_ptr<T> tmp(new T(i)); });
-	runTest("std::make_unique   ", [](const T &i) { auto tmp = std::make_unique<T>(i); });
-	runTest("boost::shared_ptr  ", [](const T &i) { boost::shared_ptr<T> tmp(new T(i)); });
-	runTest("boost::make_shared ", [](const T &i) { auto tmp = boost::make_shared<T>(i); });
+	runTest("Raw ptr new/delete ", [](const size_t &i) { T* tmp = new T(i); delete tmp; });
+	runTest("std::shared_ptr    ", [](const size_t &i) { std::shared_ptr<T> tmp(new T(i)); });
+	runTest("std::make_shared   ", [](const size_t &i) { auto tmp = std::make_shared<T>(i); });
+	runTest("std::unique_ptr    ", [](const size_t &i) { std::unique_ptr<T> tmp(new T(i)); });
+	runTest("std::make_unique   ", [](const size_t &i) { auto tmp = std::make_unique<T>(i); });
+	runTest("boost::shared_ptr  ", [](const size_t &i) { boost::shared_ptr<T> tmp(new T(i)); });
+	runTest("boost::make_shared ", [](const size_t &i) { auto tmp = boost::make_shared<T>(i); });
 }
