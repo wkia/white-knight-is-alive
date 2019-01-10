2ea9deaf01e69ebd5d2eda87a1b8ee3ad024e2c3
w.k.i.a <wkia@users.noreply.github.com>
2019-01-10 19:21:54 +0300

    Added tuples, std::tie and C++17 structured bindings

--- examples/nrvo_static.cpp
+++ examples/nrvo_static.cpp
@@ -1,7 +1,13 @@
+// rvo_test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
 #include <iostream>
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <tuple>
 
 typedef std::vector<int> int_vector;
 typedef int_vector Base;
@@ -9,11 +15,12 @@ typedef int_vector Base;
 class RVO : protected Base
 {
 public:
+	using Base::push_back;
 	RVO()
 	{
 		std::cout << this << " ctor\n";
 	}
-	
+
 	~RVO()
 	{
 		std::cout << this << " dtor\n";
@@ -60,19 +67,13 @@ RVO &ReturnSelfRef(RVO &r)
 	return r;
 }
 
-RVO testRVO0(int value, size_t size, const void **localVec)
+RVO testRVO0(int value, size_t size)
 {
 	std::cout << __FUNCTION__ << std::endl;
 	return RVO(size, value);
 }
 
-RVO testRVO_internal_move(int value, size_t size, const void **localVec)
-{
-	std::cout << __FUNCTION__ << std::endl;
-	return std::move(RVO(size, value));
-}
-
-RVO testRVO1(int value, size_t size, const void **)
+RVO testRVO1(int value, size_t size)
 {
 	std::cout << __FUNCTION__ << std::endl;
 	if (0 == (value & 1))
@@ -85,78 +86,85 @@ RVO testRVO1(int value, size_t size, const void **)
 	}
 }
 
-RVO testNRVO0(int value, size_t size, const void **localVec)
+RVO testNRVO0(int value, size_t size)
 {
 	std::cout << __FUNCTION__ << std::endl;
 	RVO vec(size, value);
-	*localVec = &vec;
+	//*localVec = &vec;
 	return vec;
 }
 
-RVO testNRVO0_ref(int value, size_t size, const void **localVec)
+RVO testNRVO0_ref(int value, size_t size)
 {
 	std::cout << __FUNCTION__ << std::endl;
 	RVO vec(size, value);
-	*localVec = &vec;
+	//*localVec = &vec;
 	return ReturnSelfRef(vec);
 }
 
-RVO testNRVO1(int value, size_t size, const void **localVec)
+RVO testNRVO1(int value, size_t size)
 {
 	std::cout << __FUNCTION__ << std::endl;
 	RVO vec;
 	if (0 == value)
 	{
-		vec = RVO(size * 2, value);
-
-		*localVec = &vec;
+		vec.push_back(value);
+		//*localVec = &vec;
 		return vec;
 	}
-	else
-	{
-		vec = RVO(size, value);
 
-		*localVec = &vec;
-		return vec;
-	}
+	vec.push_back(value + 2);
+	//*localVec = &vec;
+	return vec;
 }
 
-RVO testNRVO2(int value, size_t size, const void **localVec)
+RVO testNRVO2(int value, size_t size)
 {
 	std::cout << __FUNCTION__ << std::endl;
 	RVO vec;
+	//*localVec = &vec;
 	if (0 == value)
 	{
-		vec = RVO(size * 2, value);
-
-		*localVec = &vec;
+		vec.push_back(value);
 	}
 	else
 	{
-		vec = RVO(size, value);
-
-		*localVec = &vec;
+		vec.push_back(value + 2);
 	}
 	return vec;
 }
 
-RVO testNRVO3(int value, size_t size, const void **localVec)
+RVO testNRVO3(int value, size_t size)
 {
 	std::cout << __FUNCTION__ << std::endl;
 	if (0 == value)
 	{
 		RVO vec(size * 2, value);
 
-		*localVec = &vec;
+		//*localVec = &vec;
 		return vec;
 	}
 	else
 	{
 		RVO vec(size, value);
 
-		*localVec = &vec;
+		//*localVec = &vec;
 		return vec;
 	}
+}
+
+std::tuple<RVO, int> testTuple(int value, size_t size)
+{
+	std::cout << __FUNCTION__ << std::endl;
+	RVO vec(size, value);
+	return std::make_tuple(vec, value);
+}
+
+std::tuple<RVO, int> testTupleWithMove(int value, size_t size)
+{
+	std::cout << __FUNCTION__ << std::endl;
+	RVO vec(size, value);
+	return std::make_tuple(std::move(vec), value);
 }
 
 int main()
@@ -168,81 +176,94 @@ int main()
 	{
 		std::cout << "\n=----\n";
 		const void *localVec = 0;
-		RVO vec = testRVO0(value, size, &localVec);
-		std::cout << "-----\n";
-	}
-	{
-		std::cout << "\n=---- internal move\n";
-		const void *localVec = 0;
-		RVO vec = testRVO_internal_move(value, size, &localVec);
-		std::cout << "-----\n";
-	}
-	{
-		std::cout << "\n=---- external move\n";
-		const void *localVec = 0;
-		RVO vec = std::move(testRVO0(value, size, &localVec));
-		std::cout << "-----\n";
-	}
-	{
-		std::cout << "\n=---- internal+external move\n";
-		const void *localVec = 0;
-		RVO vec = std::move(testRVO_internal_move(value, size, &localVec));
+		const RVO &vec = testRVO0(value, size);
 		std::cout << "-----\n";
 	}
 	{
 		std::cout << "\n=----\n";
 		const void *localVec = 0;
-		RVO vec = testRVO1(value, size, &localVec);
+		const RVO &vec = testRVO1(0, size);
 		std::cout << "-----\n";
 	}
 	{
 		std::cout << "\n=----\n";
 		const void *localVec = 0;
-		RVO vec = testNRVO0(value, size, &localVec);
-		if (&vec == localVec)
-			std::cout << "NRVO was applied" << std::endl;
-		else
-			std::cout << "NRVO was not applied" << std::endl;
+		const RVO &vec = testRVO1(1, size);
 		std::cout << "-----\n";
 	}
 	{
 		std::cout << "\n=----\n";
 		const void *localVec = 0;
-		RVO vec = testNRVO0_ref(value, size, &localVec);
-		if (&vec == localVec)
-			std::cout << "NRVO was applied" << std::endl;
-		else
-			std::cout << "NRVO was not applied" << std::endl;
+		const RVO &vec = testNRVO0(value, size);
 		std::cout << "-----\n";
 	}
 	{
 		std::cout << "\n=----\n";
 		const void *localVec = 0;
-		RVO vec = testNRVO1(value, size, &localVec);
-		if (&vec == localVec)
-			std::cout << "NRVO was applied" << std::endl;
-		else
-			std::cout << "NRVO was not applied" << std::endl;
+		const RVO &vec = testNRVO0_ref(0, size);
 		std::cout << "-----\n";
 	}
 	{
 		std::cout << "\n=----\n";
 		const void *localVec = 0;
-		RVO vec = testNRVO2(value, size, &localVec);
-		if (&vec == localVec)
-			std::cout << "NRVO was applied" << std::endl;
-		else
-			std::cout << "NRVO was not applied" << std::endl;
+		const RVO &vec = testNRVO1(0, size);
 		std::cout << "-----\n";
 	}
 	{
 		std::cout << "\n=----\n";
 		const void *localVec = 0;
-		RVO vec = testNRVO3(value, size, &localVec);
-		if (&vec == localVec)
-			std::cout << "NRVO was applied" << std::endl;
-		else
-			std::cout << "NRVO was not applied" << std::endl;
+		const RVO &vec = testNRVO1(1, size);
+		std::cout << "-----\n";
+	}
+	{
+		std::cout << "\n=----\n";
+		const void *localVec = 0;
+		const RVO &vec = testNRVO2(0, size);
+		std::cout << "-----\n";
+	}
+	{
+		std::cout << "\n=----\n";
+		const void *localVec = 0;
+		const RVO &vec = testNRVO2(1, size);
+		std::cout << "-----\n";
+	}
+	{
+		std::cout << "\n=----\n";
+		const void *localVec = 0;
+		const RVO &vec = testNRVO3(0, size);
+		std::cout << "-----\n";
+	}
+	{
+		std::cout << "\n=----\n";
+		const void *localVec = 0;
+		const RVO &vec = testNRVO3(1, size);
+		std::cout << "-----\n";
+	}
+	{
+		std::cout << "\n=----\n";
+		const void *localVec = 0;
+		auto res = testTuple(1, size);
+		const RVO &vec = std::get<RVO>(res);
+		std::cout << "-----\n";
+	}
+	{
+		std::cout << "\n=----\n";
+		const void *localVec = 0;
+		auto res = testTupleWithMove(1, size);
+		const RVO &vec = std::get<0>(res);
+		std::cout << "-----\n";
+	}
+	{
+		std::cout << "\n=---- using C++14 std::tie\n";
+		const void *localVec = 0;
+		RVO vec;
+		std::tie(vec, std::ignore) = testTupleWithMove(1, size);
+		std::cout << "-----\n";
+	}
+	{
+		std::cout << "\n=---- using C++17 structured binding\n";
+		const void *localVec = 0;
+		auto [vec, val] = testTuple(1, size);
 		std::cout << "-----\n";
 	}
 }
